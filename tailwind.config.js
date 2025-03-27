149aad3ad0fea3169ad4460d7f5d97902d0f7829
Aron Asor <hello@aron.asorman.io>
2025-03-26 22:12:55 -0700

    feat: update font and background to match write.as

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./layouts/**/*.html", "./content/**/*.md", "./layouts/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Lora', 'Palatino Linotype', 'Book Antiqua', 'New York', 'DejaVu serif', 'serif'],
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
